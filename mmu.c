d4a0407bd56183bf08b37c431d722d85faaf1e64
TheoReyes <theodore.anthony.reyes@gmail.com>
2026-05-29 22:17:29 -0700

    Implemented basics of write8, xram/oam/hram

--- mmu.c
+++ mmu.c
@@ -6,6 +6,9 @@
 
 static uint8_t wram[0x2000];   // 8KiB Work Ram
 static uint8_t vram[0x2000];   // 8KiB Video Ram
+static uint8_t xram[0x2000];   // 8KiB Cartridge/External Ram
+static uint8_t hram[0x127];    // High-Ram (127 bytes)
+static uint8_t oam[0xA0];      // Object-Attribute Memory (160 bytes)
 static uint8_t *rom;           // ROM
 
 uint8_t read8(uint16_t addr) {
@@ -24,24 +27,70 @@ uint8_t read8(uint16_t addr) {
             return wram[addr - 0xC000];
         case 0xD000 ... 0xDFFF: // 4  KiB WRAM (CGB mode: bank 1-7)
             return wram[addr - 0xC000];
-        case 0xE000 ... 0xFDFF: // Echo RAM, Mirror of C000-DDFF 
+        case 0xE000 ... 0xFDFF: // Echo RAM, Mirror of C000-DDFF
             return wram[addr - 0xE000];
-        case 0xFE00 ... 0xFE9F: // Object attribute memory (OAM)
-            // TODO: Implement  
-            return 0; 
+        case 0xFE00 ... 0xFE9F: // Object-Attribute memory (OAM)
+            // TODO: Implement lock on this later
+            return oam[addr - 0xFE00];
         case 0xFEA0 ... 0xFEFF: // Not Usable
-            return 0; 
+            // TODO: Return $FF when OAM is blocked (add check), otherwise 
+            //       return 0
+            return 0;
         case 0xFF00 ... 0xFF7F: // I/O Registers
             // TODO: Implement  
+            
             return 0; 
         case 0xFF80 ... 0xFFFE: // High RAM
-            // TODO: Implement  
-            return 0; 
+            return hram[addr - 0xFF80];
         case 0xFFFF:            // Interrupt Enable register (IE)
             // TODO: Implement  
             return 0; 
         default:
             return 0;
+    }
+}
+
+void write8(uint16_t addr, uint8_t val) {
+    switch(addr) {
+        case 0x0000 ... 0x3FFF: // 16 KiB ROM bank 00
+            // TODO: Implement MBC handler
+            return;
+        case 0x4000 ... 0x7FFF: // 16 KiB ROM bank 01-NN
+            // TODO: Implement switchable bank / mapper
+            return; 
+        case 0x8000 ... 0x9FFF: // 8  KiB VRAM
+            vram[addr - 0x800] = val;
+            return;
+        case 0xA000 ... 0xBFFF: // 8  KiB Cartridge RAM
+            xram[addr - 0xA000]= val;
+            return; 
+        case 0xC000 ... 0xCFFF: // 4  KiB WRAM
+            wram[addr - 0XC000] = val;
+            return;
+        case 0xD000 ... 0xDFFF: // 4  KiB WRAM (CGB mode: bank 1-7)
+            // TODO: Add check for CGB mode later
+            wram[addr - 0xD000] = val;
+            return;
+        case 0xE000 ... 0xFDFF: // Echo RAM, Mirror of C000-DDFF 
+            wram[addr - 0xE000] = val;
+            return;
+        case 0xFE00 ... 0xFE9F: // Object attribute memory (OAM)
+            // TODO: Implement  
+            oam[addr - 0xFE00] = val;
+            return;
+        case 0xFEA0 ... 0xFEFF: // Not Usable
+            return; 
+        case 0xFF00 ... 0xFF7F: // I/O Registers
+            // TODO: Implement  
+            return; 
+        case 0xFF80 ... 0xFFFE: // High RAM
+            hram[addr - 0xFF80] = val;
+            return;
+        case 0xFFFF:            // Interrupt Enable register (IE)
+            // TODO: Implement  
+            return; 
+        default:
+            return;
     }
 }
 
@@ -59,7 +108,7 @@ void mmu_load_rom(const char *path) {
     fclose(f);
 
     // Prints title bytes 
-    printf("Title:\t");
+    printf("Title:\t\t");
     for (int i = 0x0134; i <= 0x0143; i++) {
         if (rom[i] == '\0') break;
         printf("%c", rom[i]);
--- mmu.h
+++ mmu.h
@@ -2,6 +2,7 @@
 #define MMU_H
 
     uint8_t read8(uint16_t addr);
+    void write8(uint16_t addr, uint8_t val);
     void mmu_load_rom(const char *path);
 
 #endif
